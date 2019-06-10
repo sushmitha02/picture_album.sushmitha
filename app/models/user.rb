d6cdfb4366559a7f737dfb75ae67b0575bcc0280
sushmitha02 <sushmithahgowda27@gmail.com>
2019-06-10 16:11:29 +0530

    Active Storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         has_many :picture_album
+  has_many :picture_album
 end
